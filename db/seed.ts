db054bff9a82d7b05d968817a53bb612856c174c
Leonard Greulich <leonard.greulich@gmail.com>
2024-10-05 21:55:19 +0200

    style: update prose sizing

--- db/seed.ts
+++ db/seed.ts
@@ -3,6 +3,6 @@ import { db, Document } from "astro:db";
 export default async function() {
   await db.insert(Document).values([
     { id: "1", name: "Test-1", markdown: "Value-1" },
-    { id: "2", name: "Test-2", markdown: "# Überschrift\n## Unterschrift\nEin **Text**." },
+    { id: "2", name: "Test-2", markdown: "# Überschrift\n## Unterschrift\n### Überschrift\nEin **Text**." },
   ]);
 }
