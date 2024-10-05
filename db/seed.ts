aa3247cbaeaf56ec76d466c0fa0ac16ab8874b9b
Leonard Greulich <leonard.greulich@gmail.com>
2024-10-05 22:34:19 +0200

    style: ensure same height for grid elements

--- db/seed.ts
+++ db/seed.ts
@@ -2,7 +2,7 @@ import { db, Document } from "astro:db";
 
 export default async function() {
   await db.insert(Document).values([
-    { id: "1", name: "Test-1", markdown: "Value-1" },
+    { id: "1", name: "Test-1", markdown: "" },
     { id: "2", name: "Test-2", markdown: "# Überschrift\n## Unterschrift\n### Überschrift\nEin **Text**." },
   ]);
 }
