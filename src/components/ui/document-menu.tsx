c38ba9f6347938c2553816cfc03e0b033ee84032
Leonard Greulich <leonard.greulich@gmail.com>
2024-10-06 12:53:15 +0200

    feat: add document dropdown menu

---
+++ src/components/ui/document-menu.tsx
@@ -0,0 +1,39 @@
+import { Ellipsis, History, Trash } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export const DocumentMenu = ({ id }: { id: string }) => {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button size="icon" variant="ghost" className="h-6 w-6">
+                    <Ellipsis className="h-4 w-4" />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>
+                    Options
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <a href={id + "/versions"}>
+                    <DropdownMenuItem className="cursor-pointer">
+                        <History className="mr-2 h-4 w-4" />
+                        Versions
+                    </DropdownMenuItem>
+                </a>
+                <DropdownMenuItem>
+                    <Trash className="mr-2 h-4 w-4" />
+                    Delete
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
