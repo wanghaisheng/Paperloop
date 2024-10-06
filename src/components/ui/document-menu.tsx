53af6f58380bd6b9f81829473dbb29a7d448e6d5
Leonard Greulich <leonard.greulich@gmail.com>
2024-10-06 19:05:27 +0200

    refactor: remove dropdown menu for document options

--- src/components/ui/document-menu.tsx
+++ src/components/ui/document-menu.tsx
@@ -1,16 +1,7 @@
 import { actions } from "astro:actions";
 import { navigate } from "astro:transitions/client";
-import { Ellipsis, History, Trash } from "lucide-react";
+import { History, Trash } from "lucide-react";
 import { Button } from "@/components/ui/button";
-
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 
 import {
     AlertDialog,
@@ -26,52 +17,39 @@ import {
 
 export const DocumentMenu = ({ id }: { id: string }) => {
     return (
-        <AlertDialog>
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
+        <div className="flex gap-2">
+            <a href={id + "/versions"}>
+                <Button size="icon" variant="ghost" className="h-6 w-6">
+                    <History className="h-4 w-4" />
+                </Button>
+            </a>
+            <AlertDialog>
+                <AlertDialogTrigger asChild>
                     <Button size="icon" variant="ghost" className="h-6 w-6">
-                        <Ellipsis className="h-4 w-4" />
+                        <Trash className="h-4 w-4" />
                     </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                    <DropdownMenuLabel>
-                        Options
-                    </DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <a href={id + "/versions"}>
-                        <DropdownMenuItem className="cursor-pointer">
-                            <History className="mr-2 h-4 w-4" />
-                            Versions
-                        </DropdownMenuItem>
-                    </a>
-                    <AlertDialogTrigger asChild>
-                        <DropdownMenuItem className="cursor-pointer">
-                            <Trash className="mr-2 h-4 w-4" />
-                            Delete
-                        </DropdownMenuItem>
-                    </AlertDialogTrigger>
-                </DropdownMenuContent>
-            </DropdownMenu>
-            <AlertDialogContent>
-                <AlertDialogHeader>
-                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                    <AlertDialogDescription>
-                        This action cannot be undone. This will permanently delete your
-                        account and remove your data from our servers.
-                    </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                    <AlertDialogCancel>
-                        Cancel
-                    </AlertDialogCancel>
-                    <AlertDialogAction onClick={async () => {
-                        const { error } = await actions.deleteDocument(id);
-                        if (!error) setTimeout(() => navigate("/docs"), 250);
-                    }}>
-                        Continue
-                    </AlertDialogAction>
-                </AlertDialogFooter>
-            </AlertDialogContent>
-        </AlertDialog>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                        <AlertDialogDescription>
+                            This action cannot be undone. This will permanently delete your
+                            account and remove your data from our servers.
+                        </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel>
+                            Cancel
+                        </AlertDialogCancel>
+                        <AlertDialogAction onClick={async () => {
+                            const { error } = await actions.deleteDocument(id);
+                            if (!error) setTimeout(() => navigate("/docs"), 250);
+                        }}>
+                            Continue
+                        </AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
+        </div>
     );
 };
