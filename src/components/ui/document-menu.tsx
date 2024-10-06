5f1db2a38cdd8ff1196fa78f306267971b0ae933
Leonard Greulich <leonard.greulich@gmail.com>
2024-10-06 18:55:30 +0200

    feat: add alert dialog to confirm deletion

--- src/components/ui/document-menu.tsx
+++ src/components/ui/document-menu.tsx
@@ -10,35 +10,68 @@ import {
     DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
+
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from "@/components/ui/alert-dialog";
 
 export const DocumentMenu = ({ id }: { id: string }) => {
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button size="icon" variant="ghost" className="h-6 w-6">
-                    <Ellipsis className="h-4 w-4" />
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuLabel>
-                    Options
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <a href={id + "/versions"}>
-                    <DropdownMenuItem className="cursor-pointer">
-                        <History className="mr-2 h-4 w-4" />
-                        Versions
-                    </DropdownMenuItem>
-                </a>
-                <DropdownMenuItem onClick={async () => {
-                    const { error } = await actions.deleteDocument(id);
-                    if (!error) navigate("/docs");
-                }}>
-                    <Trash className="mr-2 h-4 w-4" />
-                    Delete
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <AlertDialog>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button size="icon" variant="ghost" className="h-6 w-6">
+                        <Ellipsis className="h-4 w-4" />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>
+                        Options
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <a href={id + "/versions"}>
+                        <DropdownMenuItem className="cursor-pointer">
+                            <History className="mr-2 h-4 w-4" />
+                            Versions
+                        </DropdownMenuItem>
+                    </a>
+                    <AlertDialogTrigger asChild>
+                        <DropdownMenuItem className="cursor-pointer">
+                            <Trash className="mr-2 h-4 w-4" />
+                            Delete
+                        </DropdownMenuItem>
+                    </AlertDialogTrigger>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This action cannot be undone. This will permanently delete your
+                        account and remove your data from our servers.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>
+                        Cancel
+                    </AlertDialogCancel>
+                    <AlertDialogAction onClick={async () => {
+                        const { error } = await actions.deleteDocument(id);
+                        if (!error) setTimeout(() => navigate("/docs"), 250);
+                    }}>
+                        Continue
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
     );
 };
