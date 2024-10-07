8d37924764c0f909a65d7b7191908ee12b0a808d
Leonard Greulich <leonard.greulich@gmail.com>
2024-10-07 16:31:23 +0200

    feat: render edit and publish actions in versions view

--- src/components/ui/document-menu.tsx
+++ src/components/ui/document-menu.tsx
@@ -1,6 +1,6 @@
 import { actions } from "astro:actions";
 import { navigate } from "astro:transitions/client";
-import { History, Trash } from "lucide-react";
+import { History, Pen, CircleCheck, Trash } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 import {
@@ -13,16 +13,30 @@ import {
     AlertDialogHeader,
     AlertDialogTitle,
     AlertDialogTrigger,
-  } from "@/components/ui/alert-dialog";
+} from "@/components/ui/alert-dialog";
 
-export const DocumentMenu = ({ id }: { id: string }) => {
+export const DocumentMenu = ({ id, version }: { id: string, version: number | null }) => {
+    const versionsView = window.location.pathname.includes("versions");
     return (
         <div className="flex gap-2" onClick={event => event.preventDefault()}>
-            <Button size="icon" variant="ghost" className="h-6 w-6" onClick={() => {
-                navigate(`/docs/${id}/versions`);
-            }}>
-                <History className="h-4 w-4" />
-            </Button>
+            {!versionsView
+                ? <Button size="icon" variant="ghost" className="h-6 w-6" onClick={() => {
+                    navigate(`/docs/${id}/versions`);
+                }}>
+                    <History className="h-4 w-4" />
+                </Button>
+                : version
+                    ? <Button size="icon" variant="ghost" className="h-6 w-6" onClick={() => {
+                        navigate(`/docs/${id}/versions`);
+                    }}>
+                        <Pen className="h-4 w-4" />
+                    </Button>
+                    : <Button size="icon" variant="ghost" className="h-6 w-6" onClick={() => {
+                        navigate(`/docs/${id}/versions`);
+                    }}>
+                        <CircleCheck className="h-4 w-4" />
+                    </Button>
+            }
             <AlertDialog>
                 <AlertDialogTrigger asChild>
                     <Button size="icon" variant="ghost" className="h-6 w-6">
