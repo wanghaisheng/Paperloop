526d716ae0bfd4875beb12bca357fbd8a5047353
Leonard Greulich <leonard.greulich@gmail.com>
2024-10-08 16:59:01 +0200

    build: add clerk dependency

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import db from "@astrojs/db";
+import clerk from "@clerk/astro";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
@@ -14,6 +15,7 @@ export default defineConfig({
     },
     integrations: [
         db(),
+        clerk(),
         react(),
         tailwind({
             applyBaseStyles: false
