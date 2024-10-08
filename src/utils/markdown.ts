bd7efe6dc8a4503917a6b4e741f669b9da6aae67
Leonard Greulich <leonard.greulich@gmail.com>
2024-10-08 12:00:26 +0200

    feat: add document render page

---
+++ src/utils/markdown.ts
@@ -0,0 +1,27 @@
+import markdownit from "markdown-it";
+
+const md = markdownit({
+    html: true,
+    breaks: true
+});
+
+export const markdownToHTML = (value: string, values: Record<string, string>) => {
+    // TODO: Evaluate whether to create markdown-it plugin
+    const replaced = value.replace(/\[(\S+)\]/g, (_, key) => {
+        const value = values[key] ?? String();
+        return `<content-editable value="${value}" placeholder="${key}"></content-editable>`;
+    });
+
+    // TODO: Evaluate whether to create markdown-it plugin
+    const html = md.render(replaced);
+    const parsed = new DOMParser().parseFromString(html, "text/html");
+    for (const element of parsed.querySelectorAll("h1, h2, h3, p")) {
+        const [match, tailwind] = element.textContent?.match(/^([a-z0-9-]+):/) ?? [];
+        if (!match) continue;
+
+        element.innerHTML = element.innerHTML.replace(match, String());
+        element.classList.add(tailwind);
+    }
+
+    return new XMLSerializer().serializeToString(parsed);
+};
