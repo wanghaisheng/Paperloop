9499618394d180e2266762a90b771a106a974136
Leonard Greulich <leonard.greulich@gmail.com>
2024-10-12 22:17:48 +0200

    refactor: minor code refactoring

--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -3,16 +3,10 @@ import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ url, cookies, redirect }) => {
     const authCode = url.searchParams.get("code");
-
-    if (!authCode) {
-        return new Response("No code provided", { status: 400 });
-    }
+    if (!authCode) return redirect("/auth");
 
     const { data, error } = await supabase.auth.exchangeCodeForSession(authCode);
-
-    if (error) {
-        return new Response(error.message, { status: 500 });
-    }
+    if (error) return redirect("/auth");
 
     const { access_token, refresh_token } = data.session;
     cookies.set("sb-access-token", access_token, { path: "/" });
