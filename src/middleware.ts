563282e5c4a19b26e0e910546f2be1af7b754a89
Leonard Greulich <leonard.greulich@gmail.com>
2024-10-13 10:11:17 +0200

    refactor: move auth redirect from middleware to page

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,13 +6,6 @@ export const onRequest = defineMiddleware(async (context, next) => {
     const refreshToken = context.cookies.get("sb-refresh-token");
 
     if (accessToken && refreshToken) {
-        if (
-            context.request.method === "GET"
-            && ["login", "register"].some(path => context.url.pathname.includes(path))
-        ) {
-            return context.redirect("/docs");
-        }
-
         const { data, error } = await supabase.auth.setSession({
             refresh_token: refreshToken.value,
             access_token: accessToken.value,
